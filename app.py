1506e2f7dc8480ccbd8ba26bb70afcf19b04e4be
prathamPhonepe <pratham.intern@phonepe.com>
2025-01-15 16:56:03 +0530

    minor update on app.py - abt threads

--- app.py
+++ app.py
@@ -1,20 +1,3 @@
-# from flask import Flask
-# from routes.routes import init_routes 
-# from flask_cors import CORS
-
-
-# app = Flask(__name__)
-
-
-# CORS(app)
-
-# app.register_blueprint(init_routes(), url_prefix='/api/v1')
-
-# if __name__ == "__main__":
-#     app.run(debug=True, port=3000)
-
-
-
 import sys
 import os
 from flask import Flask
