c881eef3b05c63aff8717e51cdea8de2b6dc7022
prathamPhonepe <pratham.intern@phonepe.com>
2025-01-20 11:39:39 +0530

    Refactor import statements in user_model.py for improved clarity and organization

--- models/user_model.py
+++ models/user_model.py
@@ -1,8 +1,9 @@
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from config.config import  getConnection , closeConnection
 
-from config.config import closeConnection , getConnection
+
 
 
 # class UserModel:
